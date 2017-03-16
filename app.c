e44ea75302674c076dd16de6cd7a34f1d8d20c57
Fred Zhang <fredzqm@gmail.com>
2017-03-16 13:16:42 -0400

    formating the data sent with a first type bit

--- app.c
+++ app.c
@@ -1,21 +1,18 @@
 #include <stdlib.h>
 #include "app.h"
 
-typedef struct 
-{
-    char name[100];
-    FILE* file;
-} FileInfo;
+#define BUFFER_SIZE 1024
+#define FILENAME_BUFFER_SIZE 128
 
 
-static char name[MAX_STRING_LEN];
+#define EXIT 0
+#define MESSAGE 1
+#define NAME 10
+#define FILENAME 2
+#define FILEDATA 3
+#define FILEEND 4
 
-static void requestName(char* buffer) {
-    printf("Provide user name: ");
-    if (fgets(buffer, MAX_STRING_LEN, stdin) == NULL)
-        perror("Fail to get name");
-    buffer[strlen(buffer)-1] = 0;
-}
+static char name[BUFFER_SIZE];
 
 static void printPrompt() {
     printf("<%s> : ", name);
@@ -31,45 +28,37 @@ static int readMessage(char* buffer, int maxSize) {
     return size;
 }
 
-static void printRecievedMessage(char* message) {
-    printf("\r                                           \r");
-    printf("%s\n", message);
-    printPrompt();
-}
-
-static int parseLoadFileName(FileInfo* info, char* buffer) {
-    if (strncmp(buffer, "load ", 5) == 0) {
-        strcpy(info->name, buffer+5);
-        info->file = fopen(info->name, "r");
-        return 1;
-    }
-    return 0;
-}
-
-
 static int processAndSend(char* buffer, int size, int (*sendData)(char*, int)) {
-    FileInfo info;
     // printf("%s\n%d\n", buffer, size);
-    if (parseLoadFileName(&info, buffer)) {
+    if (strcmp(buffer+1, "exit") == 0) {
+        buffer[0] = EXIT;
+        strcpy(buffer+1, name);
+        sendData(buffer, strlen(name) + 2);
+        exit(0);
+    } else if (strncmp(buffer+1, "load ", 5) == 0) {
+        int fileNameLen = strlen(buffer+6);
+        char fileName[fileNameLen + 1];
+        strcpy(fileName, buffer+6);
+        FILE* file = fopen(fileName, "r");
+
         // send file name
-        buffer[0] = 0;
-        buffer[1] = 1;
-        strcpy(buffer+2, info.name);
-        info.file = fopen(info.name, "r");
-        if (sendData(buffer, strlen(info.name) + 2))
+        buffer[0] = FILENAME;
+        strcpy(buffer+1, fileName);
+        if (sendData(buffer, fileNameLen + 2))
             return 1;
         // send file data
+        buffer[0] = FILEDATA;
         while (1) {
-            int size = fread(buffer, 1, MAX_STRING_LEN-2, info.file);
+            int size = fread(buffer+1, 1, BUFFER_SIZE-1, file);
             if (size <= 0)
                 return 1;
-            sendData(buffer, size+2);
+            sendData(buffer, size+1);
         }
-        // signal the end of file transfer
-        buffer[0] = 0;
-        buffer[1] = 0;
-        sendData(buffer,2);
+        // signal the end of a file
+        buffer[0] = FILEEND;
+        sendData(buffer, 1);
     } else {
+        buffer[0] = MESSAGE;
         if (sendData(buffer, size) < 0)
             return 1;
     }
@@ -80,14 +69,20 @@ static int processAndSend(char* buffer, int size, int (*sendData)(char*, int)) {
 void *send_func(void *data_struct) {
     SendDataFun* sendData = (SendDataFun*) data_struct;
     
-    requestName(name);
-
-    if (sendData(name, strlen(name)) < 0)
+    char buffer[BUFFER_SIZE];
+    buffer[0] = NAME;
+    printf("Provide user name: ");
+    if (fgets(buffer+1, BUFFER_SIZE-1, stdin) == NULL) {
+        perror("Fail to get name");
+        exit(3);
+    }
+    if (sendData(buffer, strlen(buffer+1) + 2) < 0) {
         perror("error sending name");
+        exit(2);
+    }
 
-    char buffer[MAX_STRING_LEN];
     while (1) { /* run until user enters "." to quit. */
-        int size = readMessage(buffer, MAX_STRING_LEN);
+        int size = readMessage(buffer+1, BUFFER_SIZE-1);
         if (size < 0)
             break;
         if (processAndSend(buffer, size, sendData))
@@ -96,39 +91,49 @@ void *send_func(void *data_struct) {
     return NULL;
 }
 
-
-
-static void onRecieveData(FileInfo* info, char* data, int size) {
-    if (data[0] == 0) {
-        if (data[1] == 1) {
-            if (info->name[1] == 0) {
-                strcpy(info->name, data+2);
-            } else {
-                fwrite(data, 1, size, info->file);
+static void onRecieveData(char* data, int size) {
+    if (data[0] == MESSAGE) {
+        data[size] = 0;
+        printf("\r                                           \r");
+        printf("%s\n", data+1);
+        printPrompt();
+    } else if (data[0] == NAME) {
+        printf("%s is entering the chat", data+1);
+    } else if (data[0] == FILENAME) {
+        char fileName[FILENAME_BUFFER_SIZE];
+        strcpy(fileName, data+1);
+        FILE* file = fopen(fileName, "w");
+        while(1){
+            getNextPacket(&data, &size);
+            if (size < 0) {
+                perror("size negative");
+                exit(2);
             }
-        } else {
-            // end recieving
-            info->name[0] = 0;
-            fclose(info->file);
+            if (data[0] == FILEEND)
+                break;
+            if (data[0] != FILEDATA) {
+                fprintf(stderr, "Wrong file passing prototype: %d\n", data[0]);
+                exit(2);
+            }
+            fwrite(data+1, 1, size-1, file);
+            free(data);
         }
     } else {
-        data[size] = 0;
-        printRecievedMessage(data);
+        fprintf(stderr, "Unrecognized message type %d\n", data[0]);
+        exit(2);
     }
 }
 
 void *recv_func(void *data_struct) {
-    // SendDataFun* sendData = (SendDataFun*) data_struct;
-    FileInfo info;
+    char* data;
+    int size;
     while(1){
-        char* data;
-        int size;
         getNextPacket(&data, &size);
         if (size < 0) {
             perror("size negative");
             break;
         }
-        onRecieveData(&info, data, size);
+        onRecieveData(data, size);
         free(data);
     }
     exit(0);
--- app.h
+++ app.h
@@ -1,21 +1,6 @@
 #ifndef chatRoom_IO
 #define chatRoom_IO
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <dirent.h>
-#include <errno.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#define MAX_STRING_LEN 1024
-
 #include "broadCastServer.h"
 
 void *send_func(void *data_struct);
