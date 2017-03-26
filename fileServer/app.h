771d17bc0c663321459628bfab9e336f1829b93a
Fred Zhang <fredzqm@gmail.com>
2017-03-26 01:02:56 -0400

    remove app.cpp and app.h

--- fileServer/app.h
+++
@@ -1,9 +0,0 @@
-// #ifndef chatRoom_IO
-// #define chatRoom_IO
-
-// #include "broadCastServer.h"
-
-// void *send_func(SendDataFun *data_struct);
-// void *recv_func(SendDataFun *data_struct);
-
-// #endif
